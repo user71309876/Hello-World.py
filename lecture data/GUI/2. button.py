c7f905d41f8409b950384d3cd644a650e88e53e1
user71309876 <yongdong1205@gmail.com>
2021-11-21 15:45:54 +0900

    first commit

---
+++ lecture data/GUI/2. button.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root=Tk()
+root.title("Nado GUI")#프로그램 이름 정하기
+
+btn1=Button(root,text="버튼1")#Button(어느 창에 넣을 것인지,버튼 안에 넣을 글자는 무엇인지)
+btn1.pack()
+
+root.mainloop()
--- tetris/tetris.py
+++ tetris/tetris.py
@@ -448,7 +448,7 @@ pg.quit()
 """
 남은 과업
 줄 없애는 코드 만들기
-(optional)점수 만들기
-(optional)단계 만들기
 자잘한 버그 고치기
+(optional)단계 만들기
+(optional)점수 만들기
 """
